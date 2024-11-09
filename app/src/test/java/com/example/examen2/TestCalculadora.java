b34e17e95f1d9045cd41525838ea01717f7dc59e
edu20056 <ejcanessaq@gmail.com>
2024-11-09 15:45:07 -0600

    Add- Pruebas Unitarias.
    Se realizaron 2 pruebas unitarias a cada método de la clase Calculadora.
    
    Co-Authored-By: Luis Felipe Chaves Mena  <167169513+luisfelitec-0603@users.noreply.github.com>

---
+++ app/src/test/java/com/example/examen2/TestCalculadora.java
@@ -0,0 +1,56 @@
+package com.example.examen2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestCalculadora {
+
+    //Prueba para el método convertirNumero con un caso de excepción.
+    //Intenta convertir una cadena inválida en binario, lo cual debería lanzar
+    //una excepción de formato de número y retornar -1.
+
+    @Test
+    public void testConvertirNumeroException() {
+        assertEquals(-1, Calculadora.convertirNumero("notBinary", 0));
+    }
+
+    //Prueba para el método realizarOperacion con un caso de excepción.
+    //Intenta realizar una división por cero, lo cual debería lanzar
+    //una excepción de tipo ArithmeticException.
+    @Test
+    public void testRealizarOperacionDivisionPorCero() {
+        assertThrows(ArithmeticException.class, () -> {
+            Calculadora.realizarOperacion(10, 0, "/");
+        });
+    }
+    
+     //Prueba para el método convertirADestino con un caso de excepción.
+     //Intenta convertir un número a una base no soportada, lo cual debería
+     //lanzar una excepción de tipo IllegalArgumentException.
+
+    @Test
+    public void testConvertirADestinoBaseNoSoportada() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Calculadora.convertirADestino(10, 5);
+        });
+    }
+
+    // Test - convertirNumero
+    @Test
+    public void testConvertirNumero() {
+        assertEquals(5, Calculadora.convertirNumero("101", 0));
+    }
+
+    // Test - realizarOperacion
+    @Test
+    public void testRealizarOperacion() {
+        assertEquals(9, Calculadora.realizarOperacion(5, 4, "+"));
+    }
+
+    // Test - convertirADestino
+    @Test
+    public void testConvertirADestino() {
+        assertEquals("ff", Calculadora.convertirADestino(255, 3));
+    }
+}
